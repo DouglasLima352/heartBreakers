b43cf759b99326d8d5cb15fcafc5403bcb99c298
brngom3s <102827324+brngom3s@users.noreply.github.com>
2023-10-03 11:50:38 -0300

    doctor_login
    innitial doctor login

---
+++ src/model/Doctor.java
@@ -0,0 +1,31 @@
+package model;
+
+public class Doctor {
+
+	private int id_doctor;
+	private String cpf_doctor, senha_doctor;
+	
+	public int getId_doctor() {
+		return id_doctor;
+	}
+	
+	public void setId_doctor() {
+		this.id_doctor = id_doctor;
+	}
+	
+	public String getCpf_doctor() {
+		return cpf_doctor;
+	}
+	
+	public void setCpf_doctor() {
+		this.cpf_doctor = cpf_doctor;
+	}
+	
+	public String getSenha_doctor() {
+		return senha_doctor;
+	}
+	
+	public void setSenha_doctor() {
+		this.senha_doctor = senha_doctor;
+	}
+}
