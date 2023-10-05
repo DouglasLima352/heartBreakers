5c082fea795fb545f63d00ba35f76f4ae3216e8b
brunagtmaia <brunagmaia05@gmail.com>
2023-10-05 08:21:25 -0300

    Update PacienteHB

--- src/Controllers/Controller.java
+++ src/Controllers/Controller.java
@@ -11,8 +11,14 @@ import dbconnection.Connect;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.*;
+
+//import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import model.Paciente;//importando a classe paciente 
@@ -20,6 +26,9 @@ import model.Paciente;//importando a classe paciente
 public class Controller {
 	@FXML
 	private TextField filterPaciente;
+	
+	@FXML
+	private VBox ListaPacientes;
 	
 	
 	//classe para fechar a tela - sair 
@@ -30,7 +39,56 @@ public class Controller {
 	}
 	//botoão para pesquisar 
 	public void FilterPaciente(ActionEvent event) throws SQLException {
-		
+		try {
+			Connection con = dbconnection.Connect.fazer_conexao();
+			
+			List<Paciente> pacientes = new ArrayList<>();
+			
+			String sql = "select * from patient where name=?";
+			PreparedStatement stmt = con.prepareStatement(sql);
+			
+			stmt.setString(1, filterPaciente.getText());
+            ResultSet rs = stmt.executeQuery();
+			
+            while (rs.next()) {
+				//esta estrutura está pasicamente percorrendo o banco e adicionando esses valores a lista que será usada para exibir um resultado 
+                Paciente paciente = new Paciente();
+
+                paciente.setProntuarionNumber(rs.getInt("medical_record"));
+                paciente.setName(rs.getString("name"));
+                paciente.setPhoto(rs.getString("photo"));
+                pacientes.add(paciente);
+            };
+            
+            Double layoutXPane = 102.0;
+			Double layoutYPane = 14.0;
+			Double widthPane = 50.0;
+			Double heightpane = 70.0;
+			
+            int n = pacientes.size();
+            if(n==0) {
+            	System.out.print("nenhum paciente encontrado");
+            }else {
+            	for (int i=0; i<n; i++) {
+            		layoutXPane = layoutXPane + 10.0;
+    		    	Pane PacienteList = new Pane();
+    		    	PacienteList.setLayoutX(layoutXPane);
+    		    	PacienteList.setLayoutY(layoutYPane);
+    		    	PacienteList.setPrefWidth(widthPane);
+    		    	PacienteList.setPrefHeight(heightpane);
+    		    	PacienteList.setStyle("-fx-background-color: black; -fx-background-radius: 10px;");
+    		    	ListaPacientes.getChildren().add(PacienteList);
+    		    	
+    		    	System.out.print(layoutXPane + layoutYPane + widthPane + heightpane);
+    		    }
+            }
+		} catch (SQLException e1) {
+			// TODO: handle exception
+			System.out.print("catch");
+			e1.printStackTrace();
+		}
+	}
+		/*
 		Connection con = dbconnection.Connect.fazer_conexao();
 		
 		List<Paciente> pacientes = new ArrayList<>();
@@ -53,15 +111,27 @@ public class Controller {
                 pacientes.add(paciente);
             }
 			System.out.printf("Percorrendo o ArrayList (usando o índice)\n");
+			Double layoutXPane = 102.0;
+			Double layoutYPane = 14.0;
+			Double widthPane = 378.0;
+			Double heightpane = 70.0;
+			
 		    int n = pacientes.size();
 		    for (int i=0; i<n; i++) {
 		      //System.out.printf("Posição %d- %s\n", i, pacientes.get(i));
-		    	System.out.printf( "Posição %d- %s\n", i, pacientes.get(i) + " , Número do prontuário: " + pacientes.get(i).getProntuarionNumber());
+		    	System.out.printf( "Posição %d- %s\n", i, pacientes.get(i) + " , Número do prontuário: " + pacientes.get(i).getProntuarionNumber() + "pacientes: " + n);
+		    	layoutXPane = layoutXPane + 10.0;
+		    	Pane PacienteList = new Pane();
+		    	PacienteList.setLayoutX(layoutXPane);
+		    	PacienteList.setLayoutY(layoutYPane);
+		    	PacienteList.setPrefWidth(i);
+		    	PacienteList.setStyle("-fx-background-color: black; -fx-background-radius: 10px;");
+		    	ListaPacientes.getChildren().add(PacienteList);
 		    }
 		} catch (Exception ex) {
 			// TODO: handle exception
 			System.out.print("catch");
 			ex.printStackTrace();
-		}
+		}*/
 	}
-}
+
