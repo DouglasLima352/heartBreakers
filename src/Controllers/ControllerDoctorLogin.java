20da9574f93566f9e203b04df14c4a03f0cd70cc
brngom3s <102827324+brngom3s@users.noreply.github.com>
2023-10-09 08:01:00 -0300

    controller DoctorLogin update
    fix da sobreposição de tela, a primeira fecha e a segunda abre

--- src/Controllers/ControllerDoctorLogin.java
+++ src/Controllers/ControllerDoctorLogin.java
@@ -4,88 +4,75 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-
-import dbconnection.Connect;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.*;
-import javafx.stage.Stage;
 import javafx.scene.control.Button;
-
-
-import model.Doctor;// import da classe Doctor
+import javafx.stage.Stage;
+import dbconnection.Connect;
 
 public class ControllerDoctorLogin {
-	
-	@FXML
-	private TextField txtCpfDoctor;
-	
-	@FXML
-	private TextField txtSenhaDoctor;
-	
-	@FXML
-	private Button btnLogin;
-	
-	Connection conexao = null;
-	PreparedStatement pst = null;
-	ResultSet rs = null;
-	
-	public void loginDoctor (ActionEvent event) {
-		String sql = "select * from doctor where cpf=? and password=?";
-		try {
-			//conexao
-			Connection connect = dbconnection.Connect.fazer_conexao();
-			//preparo pra consulta sql
-			pst = connect.prepareStatement(sql);
-			pst.setString(1, txtCpfDoctor.getText());
-			pst.setString(2, txtSenhaDoctor.getText());
-			//resultado da consulta
-			rs = pst.executeQuery();
-			if (rs.next()) {
-				FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/PacienteHB.fxml"));
+
+    @FXML
+    private TextField txtCpfDoctor;
+
+    @FXML
+    private TextField txtSenhaDoctor;
+
+    @FXML
+    private Button btnLogin;
+
+    Connection conexao = null;
+    PreparedStatement pst = null;
+    ResultSet rs = null;
+
+    @FXML
+    public void loginDoctor(ActionEvent event) {
+        String sql = "select * from doctor where cpf=? and password=?";
+        try {
+            // conexao
+            Connection connect = dbconnection.Connect.fazer_conexao();
+            // preparo consulta sql
+            pst = connect.prepareStatement(sql);
+            pst.setString(1, txtCpfDoctor.getText());
+            pst.setString(2, txtSenhaDoctor.getText());
+            // resultado da consulta
+            rs = pst.executeQuery();
+            if (rs.next()) {
+                // pega a primeira pagina e fecha
+                Stage currentStage = (Stage) btnLogin.getScene().getWindow();
+                currentStage.close(); // 
+
+                // abre segunda pagina
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/PacienteHB.fxml"));
                 Parent root = loader.load();
                 Scene scene = new Scene(root);
-                Stage stage = new Stage();
-                stage.setScene(scene);
-                stage.setResizable(false);//impede que o stage seja redimencionado.
-                stage.show();
-			}else {
-				System.out.println("Login ou senha incorretos.");
-			}
-		} catch (Exception e) {
-			System.out.print(e);
-		}	finally { try {
-            if (rs != null) {
-                rs.close();
+                Stage secondStage = new Stage();
+                secondStage.setScene(scene);
+                secondStage.setResizable(false); // impede que o stage seja redimensionado.
+                secondStage.show();
+            } else {
+                System.out.println("Login ou senha incorretos.");
             }
-            if (pst != null) {
-                pst.close();
+        } catch (Exception e) {
+            System.out.print(e);
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (pst != null) {
+                    pst.close();
+                }
+                if (conexao != null) {
+                    conexao.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
-            if (conexao != null) {
-                conexao.close();
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
         }
-	}
-		
+    }
 }
-	
-	private void btnLoginActionPerformed(ActionEvent evt) {
-        loginDoctor(evt);
-	}
-	
-}
-
-
-	
-	
-	
-	
